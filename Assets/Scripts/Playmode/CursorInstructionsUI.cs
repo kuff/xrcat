b9813ecbad187c821fed2c7939a546d798bca5aa
kuff <16867891+kuff@users.noreply.github.com>
2024-02-06 14:55:52 +0100

    Setup for Oculus deployment, update packages

--- Assets/Scripts/Playmode/CursorInstructionsUI.cs
+++ Assets/Scripts/Playmode/CursorInstructionsUI.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 using TMPro;
 
@@ -19,6 +20,7 @@ namespace Playmode
             UpdateInstructionText();
         }
 
+        [SuppressMessage("ReSharper", "StringLiteralTypo")]
         private void UpdateInstructionText()
         {
             instructionText.text = _rotateWithCursorScript.CursorIsConfined 
